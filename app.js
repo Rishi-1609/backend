ffaa501aadd44816af645e2b53395ae083518b4e
Rishi-1609 <raorishindra16@gmail.com>
2026-02-20 20:01:45 +0530

    Update app.js for deployment

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 // app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+       res.send("Welcome to my first deployed backend");
+})
+
 const dataRoute = require('./routes/dataRoute');
 const userRoute = require('./routes/userRoute');
 const userService = require('./services/userData');
 const { exit } = require('process');
 app.use('/data', dataRoute);
-app.use('/user', userRoute);
+app.use('/users', userRoute);
 
 
 async function startServer() {
     try {
         await userService.initialize();
         app.listen(PORT, () => {
-        console.log(`Server is listening at http://localhost:${PORT}`);
+        console.log(`Server is listening at https://express-backend-zwcm.onrender.com/`);
     });}
     catch (err) {
         console.log("Error inititalizing the database: ",err);
